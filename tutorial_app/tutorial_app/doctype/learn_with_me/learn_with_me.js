6498e075410d286d53f42de03d4870cfa76407b6
jaysarna <sarnajay@gmail.com>
2023-12-15 21:16:46 +0500

    first

---
+++ tutorial_app/tutorial_app/doctype/learn_with_me/learn_with_me.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, jatin and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('learn with me', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ tutorial_app/tutorial_app/doctype/new_doc_for_learning/new_doc_for_learning.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, jatin and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('new doc for learning', {
+	// refresh: function(frm) {
+
+	// }
+});
